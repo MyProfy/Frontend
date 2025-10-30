210ba31a9810a5a8ffb3f9b17c1972f1aed99a50
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-10-30 15:03:46 +0500

    first task

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,18 +7,21 @@ import 'flag-icons/css/flag-icons.min.css';
 export const metadata: Metadata = {
   title: "MyProfy",
   description: "Platform for services and vacancies",
+  icons: {
+    icon: "/MyProfyLogo.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                <ThemeProvider>
-                    <Providers> 
-                        <main>{children}</main>
-                    </Providers>
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider>
+          <Providers> 
+            <main>{children}</main>
+          </Providers>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
